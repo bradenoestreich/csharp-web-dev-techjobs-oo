827fa25acefbbf865dc020813171541d0902d588
Braden <braden.i.oestreich@gmail.com>
2021-02-20 13:11:17 -0600

    final draft?

--- TechJobsOO/CoreCompetency.cs
+++ TechJobsOO/CoreCompetency.cs
@@ -1,52 +1,51 @@
 ﻿using System;
 namespace TechJobsOO
 {
-    public class CoreCompetency
+    public class CoreCompetency : JobField
     {
-        private int id;
-        public int Id
-        {
-            get { return id; }
-            internal set { id = Id; }
-        }
+        //private int id;
+        //public int Id
+        //{
+        //    get { return id; }
+        //    internal set { id = Id; }
+        //}
 
-        private static int nextId = 1;
+        //private static int nextId = 1;
 
-        private string value;
-        public string Value
-        {
-            get { return value; }
-            internal set { Value = value; }
-        }
+        //private string value;
+        //public string Value
+        //{
+        //    get { return value; }
+        //    internal set { Value = value; }
+        //}
 
         // TODO: Change the fields to auto-implemented properties.
 
-        public CoreCompetency()
+        //public CoreCompetency()
+        //{
+        //    id = nextId;
+        //    nextId++;
+        //}
+
+        public CoreCompetency(string value) : base(value)
         {
-            id = nextId;
-            nextId++;
         }
 
-        public CoreCompetency(string v) : this()
-        {
-            value = v;
-        }
+        //public override bool Equals(object obj)
+        //{
+        //    return obj is CoreCompetency competency &&
+        //           id == competency.id;
+        //}
 
-        public override bool Equals(object obj)
-        {
-            return obj is CoreCompetency competency &&
-                   id == competency.id;
-        }
+        //public override int GetHashCode()
+        //{
+        //    return HashCode.Combine(id);
+        //}
 
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(id);
-        }
-
-        public override string ToString()
-        {
-            return value;
-        }
+        //public override string ToString()
+        //{
+        //    return value;
+        //}
     }
    
 }
