aeee2e8e9a465ef3912fa398d3a419a54891340c
Braden <braden.i.oestreich@gmail.com>
2021-02-19 20:37:28 -0600

    TDD for ToString() method

--- TechJobsTests/JobTests.cs
+++ TechJobsTests/JobTests.cs
@@ -34,10 +34,55 @@ namespace TechJobsTests
             Job jobOne = new Job();
             Job jobTwo = new Job();
 
-            Assert.IsFalse(jobOne.Equals(jobTwo));
+            Assert.IsFalse(jobOne.Id.Equals(jobTwo.Id));
+            // a previous version had Assert.IsFalse(jobOne.Equals(jobTwo)), which is also false but not what we're trying to get at
         }
 
+        [TestMethod]
+        public void FirstJobToStringTest()
+        {
+            Job looneyTunes = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
 
+            string jobString = looneyTunes.ToString();
+
+            Assert.IsTrue(jobString.StartsWith('\n'));
+            Assert.IsTrue(jobString.EndsWith('\n'));
+
+        }
+
+        [TestMethod]
+        public void SecondJobToStringTest()
+        {
+            Job looneyTunes = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+            string jobString = looneyTunes.ToString();
+
+            string looneyTunesTestObj = $"\nID: {looneyTunes.Id}\n" +
+                $"Name: {looneyTunes.Name}\n" +
+                $"Employer: {looneyTunes.EmployerName.Value}\n" +
+                $"Location: {looneyTunes.EmployerLocation.Value}\n" +
+                $"Position Type: {looneyTunes.JobType.Value}\n" +
+                $"Core Competency: {looneyTunes.JobCoreCompetency.Value}\n";
+
+            Assert.AreEqual(jobString, looneyTunesTestObj);
+        }
+
+        [TestMethod]
+        public void ThirdJobToStringTest()
+        {
+            Job oopsieTunes = new Job("Product tester", new Employer("ACME"), new Location(""), new PositionType(""), new CoreCompetency("Persistence"));
+
+            string jobString = oopsieTunes.ToString();
+
+            string oopsieTunesTestObj = $"\nID: {oopsieTunes.Id}\n" +
+                $"Name: {oopsieTunes.Name}\n" +
+                $"Employer: {oopsieTunes.EmployerName.Value}\n" +
+                $"Location: {oopsieTunes.EmployerLocation.Value}\n" +
+                $"Position Type: {oopsieTunes.JobType.Value}\n" +
+                $"Core Competency: {oopsieTunes.JobCoreCompetency.Value}\n";
+
+            Assert.AreEqual(jobString, oopsieTunesTestObj);
+        }
 
     }
 }
